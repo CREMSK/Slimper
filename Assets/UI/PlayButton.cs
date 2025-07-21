12c58060c712a13c61049bfe454e6106d4adeb8b
Ivan Savchenko <vanjasavchenko@gmail.com>
2025-07-21 19:28:01 +0200

    builds

--- Assets/UI/PlayButton.cs
+++ Assets/UI/PlayButton.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    void Awake()
+    {
+        Cursor.visible = true;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
